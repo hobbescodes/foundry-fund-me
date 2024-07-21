aed9b3ea28ede1fc2cb533e7570039fc9939efac
hobbescodes <87732294+hobbescodes@users.noreply.github.com>
2024-07-20 20:35:53 -0500

    test(FundMe): add base tests

---
+++ test/FundMe.t.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {Test, console} from "forge-std/Test.sol";
+import {FundMe} from "../src/FundMe.sol";
+
+contract FundMeTest is Test {
+    FundMe fundMe;
+
+    address owner = address(this);
+
+    function setUp() external {
+        fundMe = new FundMe();
+    }
+
+    function testMinimumUSDIsFive() public view {
+        assertEq(fundMe.MINIMUM_USD(), 5 * 10 ** 18);
+    }
+
+    function testOwner() public view {
+        assertEq(fundMe.i_owner(), owner);
+    }
+}
